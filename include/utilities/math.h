7377d286b04cdc71a177a324dc031a7979523a26
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2024-02-20 20:03:59 -0500

    [fix] add include guards, scan functions

--- include/utilities/math.h
+++ include/utilities/math.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cmath>
 
 inline constexpr double deg2rad(double deg) {
--- include/utilities/scan.h
+++ include/utilities/scan.h
@@ -1,9 +1,19 @@
+#pragma once
+
 #include "math.h"
+#include <vector>
 
 inline int index_at_angle(float angle_in_degrees, float angle_min, float angle_increment) {
     return (int)((deg2rad(angle_in_degrees) - angle_min) / angle_increment);
 }
 
-inline int angle_at_index(int index, float angle_min, float angle_increment) {
+inline float angle_at_index(int index, float angle_min, float angle_increment) {
     return angle_min + angle_increment * index;
+}
+
+inline std::vector<float> keep_in_range(const std::vector<float> &ranges, size_t start, size_t end) {
+    std::vector<float> r(ranges);
+    std::fill(r.begin(), r.begin() + start, 0.0);
+    std::fill(r.begin() + end + 1, r.end(), 0.0);
+    return r;
 }
