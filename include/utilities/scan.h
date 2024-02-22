6f0688feee5ee7b9cb93a789a61b6edab1af26bb
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2024-02-21 20:50:05 -0500

    [fix] use math.h namespace in scan.h

--- include/utilities/scan.h
+++ include/utilities/scan.h
@@ -6,7 +6,7 @@
 namespace scan {
 
 inline constexpr int index_at_angle(float angle_in_degrees, float angle_min, float angle_increment) {
-    return (int)((deg2rad(angle_in_degrees) - angle_min) / angle_increment);
+    return (int)((math::deg2rad(angle_in_degrees) - angle_min) / angle_increment);
 }
 
 inline constexpr float angle_at_index(int index, float angle_min, float angle_increment) {
