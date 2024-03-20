dbfc59d3fa812f0943f1c81c86265cd8639bb3f9
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2024-03-20 15:30:55 -0400

    [py/params] add support for integer/double array params

--- utilities/params.py
+++ utilities/params.py
@@ -4,6 +4,18 @@ T = TypeVar("T")
 
 __all__ = ["get_param"]
 
+_TYPE_TO_VALUE = {
+    float: "double_value",
+    str: "string_value",
+    int: "integer_value",
+    bool: "bool_value",
+}
+
+_ELEMENT_TO_VALUE = {
+    float: "double_array_value",
+    int: "integer_array_value"
+}
+
 def get_param(
     node: Node,
     name: str,
@@ -25,14 +37,16 @@ def get_param(
     T
         The value of the parameter (typed)
     """
-    type_to_value = {
-        float: "double_value",
-        str: "string_value",
-        int: "integer_value",
-        bool: "bool_value"
-    }
+    param_type = type(default)
+    if param_type == list:
+        param_type_value = _ELEMENT_TO_VALUE[type(default[0])]
+    else:
+        param_type_value = _TYPE_TO_VALUE[param_type]
+
     node.declare_parameter(name, default)
-    param = node.get_parameter(name).get_parameter_value().__getattribute__(type_to_value[type(default)])
+    param = node.get_parameter(name).get_parameter_value().__getattribute__(param_type_value)
+    if param_type == list:
+        param = param.tolist()
     node.get_logger().info(f"{name.upper()}: {param}")
     return param
 
