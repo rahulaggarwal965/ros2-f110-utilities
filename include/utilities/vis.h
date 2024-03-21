95e86151fac1586af698e0f61696990f703fdffd
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2024-03-20 22:20:50 -0400

    [cpp/vis] refactor point visualiztation -> markers, vis paths and lines

--- include/utilities/vis.h
+++ include/utilities/vis.h
@@ -8,6 +8,10 @@
 #include <visualization_msgs/msg/marker.hpp>
 #include <unordered_map>
 
+// TODO(rahul): transition to Eigen
+namespace vis {
+
+namespace internal {
 
 template<typename T>
 static void _pub_to_topic(rclcpp::Node &node, const std::string &topic,  const T &msg) {
@@ -20,7 +24,39 @@ static void _pub_to_topic(rclcpp::Node &node, const std::string &topic,  const T
     }
 }
 
-namespace vis {
+static void _vis_points(
+    rclcpp::Node &node,
+    const std::string &topic,
+    const std::vector<geometry_msgs::msg::Point> &points,
+    int marker_type,
+    const std::string &frame,
+    float scale = 0.1f,
+    const std::array<float, 4> &color = {1.0f, 0.0f, 0.0f, 1.0f}
+) {
+    using Marker = visualization_msgs::msg::Marker;
+    Marker msg;
+    msg.header.stamp = node.get_clock()->now();
+    msg.header.frame_id = frame;
+    msg.ns = topic;
+    msg.id = 0;
+    msg.type = marker_type;
+    msg.action = Marker::ADD;
+
+    msg.points = points;
+
+    msg.scale.x = scale;
+    msg.scale.y = scale;
+    msg.scale.z = scale;
+
+    msg.color.r = color[0];
+    msg.color.g = color[1];
+    msg.color.b = color[2];
+    msg.color.a = color[3];
+
+    _pub_to_topic(node, topic, msg);
+}
+
+}
 
 inline void vis_scan(
     rclcpp::Node &node,
@@ -42,22 +78,7 @@ inline void vis_scan(
     msg.range_min = range_min;
     msg.range_max = range_max;
     msg.angle_max = angle_min + ranges.size() * angle_increment;
-    _pub_to_topic(node, topic, msg);
-}
-
-inline void vis_point(
-    rclcpp::Node &node,
-    const std::string &topic,
-    const std::array<float, 2> &point,
-    const std::string &frame
-) {
-    using PointStamped = geometry_msgs::msg::PointStamped;
-    PointStamped msg;
-    msg.header.stamp = node.get_clock()->now();
-    msg.header.frame_id = frame;
-    msg.point.x = point[0];
-    msg.point.y = point[1];
-    _pub_to_topic(node, topic, msg);
+    internal::_pub_to_topic(node, topic, msg);
 }
 
 inline void vis_points(
@@ -69,26 +90,58 @@ inline void vis_points(
     const std::array<float, 4> &color = {1.0f, 0.0f, 0.0f, 1.0f}
 ) {
     using Marker = visualization_msgs::msg::Marker;
-    Marker msg;
-    msg.header.stamp = node.get_clock()->now();
-    msg.header.frame_id = frame;
-    msg.ns = topic;
-    msg.id = 0;
-    msg.type = Marker::POINTS;
-    msg.action = Marker::ADD;
+    internal::_vis_points(node, topic, points, Marker::POINTS, frame, scale, color);
+}
 
-    msg.points = points;
+inline void vis_point(
+    rclcpp::Node &node,
+    const std::string &topic,
+    const geometry_msgs::msg::Point &point,
+    const std::string &frame,
+    float scale = 0.1f,
+    const std::array<float, 4> &color = {1.0f, 0.0f, 0.0f, 1.0f}
+) { vis_points(node, topic, {point}, frame, scale, color); }
 
-    msg.scale.x = scale;
-    msg.scale.y = scale;
-    msg.scale.z = scale;
+inline void vis_point(
+    rclcpp::Node &node,
+    const std::string &topic,
+    const std::array<float, 2> &point,
+    const std::string &frame,
+    float scale = 0.1f,
+    const std::array<float, 4> &color = {1.0f, 0.0f, 0.0f, 1.0f}
+) {
+    using Point = geometry_msgs::msg::Point;
+    Point p;
+    p.x = point[0];
+    p.y = point[1];
 
-    msg.color.r = color[0];
-    msg.color.g = color[1];
-    msg.color.b = color[2];
-    msg.color.a = color[3];
+    vis_point(node, topic, p, frame, scale, color);
+}
 
-    _pub_to_topic(node, topic, msg);
+inline void vis_path(
+    rclcpp::Node &node,
+    const std::string &topic,
+    const std::vector<geometry_msgs::msg::Point> &points,
+    const std::string &frame,
+    float scale = 0.1f,
+    const std::array<float, 4> &color = {1.0f, 0.0f, 0.0f, 1.0f}
+) {
+    using Marker = visualization_msgs::msg::Marker;
+    internal::_vis_points(node, topic, points, Marker::LINE_STRIP, frame, scale, color);
+}
+
+// The `lines` parameter should contain an array of points such that every unique pair
+// (0,1), (2,3), ... will form a line. This differs slightly from the python API.
+inline void vis_lines(
+    rclcpp::Node &node,
+    const std::string &topic,
+    const std::vector<geometry_msgs::msg::Point> &lines,
+    const std::string &frame,
+    float scale = 0.1f,
+    const std::array<float, 4> &color = {1.0f, 0.0f, 0.0f, 1.0f}
+) {
+    using Marker = visualization_msgs::msg::Marker;
+    internal::_vis_points(node, topic, lines, Marker::LINE_LIST, frame, scale, color);
 }
 
 }
